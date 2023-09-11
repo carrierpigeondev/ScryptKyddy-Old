d7cc87083d58b20ee20049783882b13d63f94462
carrierpigeondev <carrierpigeon.dev@gmail.com>
2023-09-11 17:40:46 -0500

    first commit

---
+++ Fahrenheit/src/fahrenheit.py
@@ -0,0 +1,5 @@
+def main() -> None:
+    print("Welcome to Fahrenheit")
+    
+if __name__ == "__main__":
+    main()
---
+++ site/script.js
@@ -0,0 +1,9 @@
+function toggleFeatureDetails(elementId) {
+    var element = document.getElementById(elementId);
+    if (element.style.display === "none") {
+      element.style.display = "block";
+    } else {
+      element.style.display = "none";
+    }
+  }
+  
